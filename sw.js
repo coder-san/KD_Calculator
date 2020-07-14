d102491799c45f41d918b0d461b628ce73d4af9c
coder-san <aloticode@gmail.com>
2020-07-14 23:22:46 +0530

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-7e0c7225866e5e7ff3a4.js"
+    "url": "webpack-runtime-b2e06e6f140df642b796.js"
   },
   {
     "url": "framework-5fc6169171e885c3b522.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "8c324b29defd227ebf0725253e28422e"
+    "revision": "b37bf15ae84ca12c00ae052a8a04644f"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-7c31e2436cade51cbcda.js"
@@ -48,7 +48,7 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/app-data.json",
-    "revision": "903174a97818c0e7c5ee49cd74fcfe7b"
+    "revision": "4ba3accaff670909747f8dff680cab44"
   },
   {
     "url": "manifest.webmanifest",
